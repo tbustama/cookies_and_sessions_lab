7a4d90627388125e10b4ea52434b30b271fbf833
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-23 11:16:33 -0800

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,12 @@ class ProductsController < ApplicationController
     end
 
     def add
-        self.cart << product_params[:product]
+        self.cart << product_params
         redirect_to home_path
     end
 
     private
     def product_params
-        params.require(:product).permit(:product)
+        params.require(:product)
     end
 end
